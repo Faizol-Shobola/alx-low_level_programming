7fa02f5621aea015bbc5097b08e382617c465932
Shobola Faizol <shobolafaizol@gmail.com>
2022-07-08 00:24:58 +0000

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (void)
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle \n";
-	puts(str);
+	puts (str);
 
-	return(0);
-}	
+	return (0);
+}
