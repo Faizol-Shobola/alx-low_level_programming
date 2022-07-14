e041b340967a79915be4df2b2a9669b4081d9709
Shobola Faizol <shobolafaizol@gmail.com>
2022-07-14 00:40:08 +0000

    A function that prints every other character of a string, starting with the first character, followed by a new line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line
  *
- * @str:string to print
+ * @str: string to print
  *
  * Return: void
  */
